3c5795c6147c7070ae4c9bd33046a46b14afd9a2
Rewandabies <rewandabies81@gmail.com>
2024-09-01 00:15:48 +0300

    Update records.c

--- records.c
+++ records.c
@@ -1,3 +1,10 @@
+/*================================================================
+*    @file             : records.c
+*    @brief            : records management high level code
+*    @author           : Rewan Dabies
+=================================================================*/
+
+/* Linking Section starts */
 #include <stdio.h>
 #include <stdlib.h>
 #include "records.h"
@@ -5,7 +12,7 @@
 #include "doctor.h"
 #include "appointment.h"
 #include "billing.h"
-
+/* Linking Section ends */
 
 void managerecords(){
 printf("Record Management System\n");
@@ -14,3 +21,10 @@ displaydoctors();
 displayappointments();
 displaybills();
 }
+
+/*==============================================================================
+
+    Date                By                  Description
+    ------------        ------------        ------------
+    31August2024        Rewan Dabies        creating records management file
+==============================================================================*/
